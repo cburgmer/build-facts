10c7370fd87c8a33837566c368de9c63f77279f2
Christoph Burgmer <christoph.burgmer@gmail.com>
2021-03-08 22:11:15 +0100

    Refactor, separate TeamCity build aggregation from sync action

--- src/buildviz/teamcity/sync_jobs.clj
+++ src/buildviz/teamcity/sync_jobs.clj
@@ -1,34 +1,13 @@
 (ns buildviz.teamcity.sync-jobs
-  (:require [buildviz.teamcity
-             [api :as api]
-             [transform :as transform]]
+  (:require [buildviz.teamcity.builds :as builds]
             [buildviz.storage :as storage]
-            [buildviz.util
-             [json :as json]
-             [url :as url]]
             [cheshire.core :as j]
-            [clj-http.client :as client]
             [clj-progress.core :as progress]
             [clj-time
              [coerce :as tc]
              [core :as t]
              [format :as tf]]
-            [clojure.string :as string]
-            [clojure.tools.logging :as log]
-            [uritemplate-clj.core :as templ]))
-
-(defn- all-builds-for-job [teamcity-url sync-start-time {:keys [id projectName name]}]
-  (let [safe-build-start-time (t/minus sync-start-time (t/millis 1))]
-    (->> (api/get-builds teamcity-url id)
-         (map (fn [build]
-                {:build build
-                 :project-name projectName
-                 :job-name name}))
-         (take-while #(t/after? (transform/parse-build-date (get-in % [:build :startDate])) safe-build-start-time)))))
-
-(defn- add-test-results [teamcity-url build]
-  (assoc build :tests (api/get-test-report teamcity-url (:id (:build build)))))
-
+            [clojure.tools.logging :as log]))
 
 (defn- store-test-results [data-dir job-name build-id test-results]
   (storage/store-testresults! data-dir job-name build-id (j/generate-string test-results)))
@@ -38,16 +17,6 @@
   (storage/store-build! data-dir job-name build-id build)
   (when-not (empty? test-results)
     (store-test-results data-dir job-name build-id test-results)))
-
-
-(defn- sync-oldest-first-to-deal-with-cancellation [builds]
-  (sort-by #(get-in % [:build :finishDate]) builds))
-
-(defn- ignore-ongoing-builds [builds]
-  (filter :result builds))
-
-(defn- stop-at-first-non-finished-so-we-can-resume-later [builds]
-  (take-while #(= "finished" (get-in % [:build :state])) builds))
 
 (defn- last-sync-date [builds]
   (when builds
@@ -59,21 +28,16 @@
         (last-sync-date builds)
         default-sync-start)))
 
-
 (defn sync-jobs [teamcity-url data-dir projects default-sync-start user-sync-start]
   (println "TeamCity" (str teamcity-url) projects)
   (let [sync-start-time (sync-start data-dir default-sync-start user-sync-start)]
     (println (format "Finding all builds for syncing (starting from %s)..."
                      (tf/unparse (:date-time tf/formatters) sync-start-time)))
-    (->> projects
-         (mapcat #(api/get-jobs teamcity-url %))
-         (mapcat #(all-builds-for-job teamcity-url sync-start-time %))
+    (->> (builds/teamcity-builds {:base-url teamcity-url
+                                  :projects projects}
+                                 sync-start-time)
          (progress/init "Syncing")
-         sync-oldest-first-to-deal-with-cancellation
-         stop-at-first-non-finished-so-we-can-resume-later
          (map (comp progress/tick
-                    (partial store data-dir)
-                    transform/teamcity-build->buildviz-build
-                    (partial add-test-results teamcity-url)))
+                    (partial store data-dir)))
          dorun
          (progress/done))))
