857e4623e1059f8ba6f9653381c4c18cbacda128
Christoph Burgmer <christoph.burgmer@gmail.com>
2021-04-13 21:58:05 +0200

    Simplify event integration and make loop faster by reading bigger chunks of data

--- src/build_facts/concourse/sse.clj
+++ src/build_facts/concourse/sse.clj
@@ -16,30 +16,31 @@
        (reduce (fn [acc [k v]]
                  (assoc acc (keyword k) (string/join (map second v)))) {})))
 
-(defn- lazy-event-stream [event-stream data close]
-  (let [byte-array (byte-array (max 1 (.available event-stream)))
-        bytes-read (.read event-stream byte-array)]
+(defn- load-events [event-stream]
+  (loop [events []
+         data nil]
+    (let [byte-array (byte-array 4096)
+          bytes-read (.read event-stream byte-array)]
 
-    (if (neg? bytes-read)
+      (if (neg? bytes-read)
+        (throw (Exception. "Premature end of event stream"))
 
-      (do (close)
-          [])
-
-      (let [data (str data (slurp byte-array))
-            es (->> (re-seq event-mask data) (map parse-event))]
-        (if (some #(= "end" (:event %)) es)
-          (do (close)
-              es)
-          (lazy-cat es (lazy-event-stream event-stream (string/replace data event-mask "") close)))))))
+        (let [new-data (str data (slurp byte-array))
+              es (->> (re-seq event-mask new-data) (map parse-event))]
+          (if (some #(= "end" (:event %)) es)
+            (concat events es)
+            (recur (concat events es) (string/replace new-data event-mask ""))))))))
 
 (defn events [base-url relative-url params]
   (log/info (format "Retrieving %s" relative-url))
   (let [cm (conn/make-regular-conn-manager {})
         response (client/get (string/join [base-url relative-url])
                              (merge params {:as :stream :connection-manager cm}))
-        event-stream ^InputStream (:body response)]
+        event-stream ^InputStream (:body response)
+        events (load-events event-stream)]
     (log/info (format "Retrieved %s: %s" relative-url (:status response)))
     ;; Work around that `(.close event-stream)` seems blocking, so close via shutdown
     ;; Looks like we are just side-stepping the closing of the CloseableHttpResponse:
     ;; https://github.com/dakrone/clj-http/blob/dd15359451645f677b3e294164cf70330b92241d/src/clj_http/core.clj#L456
-    (lazy-event-stream event-stream nil #(.shutdown cm))))
+    (.shutdown cm)
+    events))
