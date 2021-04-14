0aa68534095237a8bb3cceb1e02f1cc91442f1da
Christoph Burgmer <christoph.burgmer@gmail.com>
2021-04-14 21:46:45 +0200

    Initial unit test for event parsing

---
+++ test/build_facts/concourse/sse_test.clj
@@ -0,0 +1,19 @@
+(ns build-facts.concourse.sse-test
+  (:require [build-facts.concourse.sse :as sut]
+            [clojure.test :refer :all])
+  (:import [java.io InputStream]))
+
+(defn- mock-input-stream [stream]
+  (let [remaining-stream (atom stream)]
+    (proxy [InputStream] []
+      (read [bytes] (if-let [value-read (first @remaining-stream)]
+                      (do (->> value-read
+                               (map-indexed (fn [i c] (aset-byte bytes i c)))
+                               doall)
+                          (swap! remaining-stream rest)
+                          (count value-read))
+                      (throw (Error. "Read past last event")))))))
+
+(deftest test-concourse-sse
+  (testing "should handle only end event"
+    (sut/load-events (mock-input-stream ["event: end\n\n"]))))
