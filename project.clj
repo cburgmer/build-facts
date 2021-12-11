2fc602d6ff313ea2d809b35bea99ad96134aaf91
Christoph Burgmer <christoph.burgmer@gmail.com>
2021-12-11 15:43:28 +0100

    Upgrade leiningen dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "BSD 2-Clause"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.10.2"]
-                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/tools.logging "1.2.1"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                 [clj-http "3.12.1"]
+                 [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
-                 [cheshire "5.10.0"]
-                 [clj-commons/clj-yaml "0.7.0"]
+                 [cheshire "5.10.1"]
+                 [clj-commons/clj-yaml "0.7.107"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/tools.cli "1.0.206"]
                  [progrock "0.1.2"]
                  [uritemplate-clj "1.3.0"]
                  [wharf "0.2.0-20141115.032457-2"]]
